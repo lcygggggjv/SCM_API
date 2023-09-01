82111044d505df005b7d7b4a01137b3b2180c5d6
lcygggggjv <1743998121@qq.com>
2023-09-01 21:55:11 +0800

    ds

--- run.py
+++ run.py
@@ -1,21 +1,14 @@
 import pytest
-from config.file_path import FilePath
-import configparser
 import os
 
 
-# 运行所有
-pytest.main()
+if __name__ == '__main__':
 
-# 指定模块, -s 输出调试信息，包括打印信息 ， -v 显示更详细信息，可以放在一起 -vs
-# pytest.main(['-s', '-v', FilePath.create_material_api_path])
+    # 指定模块, -vs 输出调试信息，包括打印详细信息， --alluredir 输出json文件报告  后面输出目录
+    pytest.main(['-vs', '--alluredir', './report/temp'])
+    # 产生报告，将报告保存，./report/temp获取这个目录中的json文件进行渲染，-o report/rep  生成的html文件，保存到这个目录中 clean清除
+    os.system('allure generate ./report/temp  -0  ./report/temp --clean ')
 
-# os.path.dirname 获取当前文件的所在目录，前面所有级目录
-# root_path = os.path.dirname(os.path.abspath(__file__))
-#
-# ini_path = os.path.join(root_path, 'config', 'env.ini')
-#
-# config_parser = configparser.ConfigParser()
-#
-# config_parser.read(ini_path, encoding='utf-8')
+
+
 

77f7e5e61d77cc9763bb63a3ba7fb5d3bc2018a8
lcygggggjv <1743998121@qq.com>
2023-09-03 00:52:07 +0800

    12399

--- run.py
+++ run.py
@@ -9,6 +9,3 @@ if __name__ == '__main__':
     # 产生报告，将报告保存，./report/temp获取这个目录中的json文件进行渲染，-o report/rep  生成的html文件，保存到这个目录中 clean清除
     os.system('allure generate ./report/temp  -0  ./report/temp --clean ')
 
-
-
-
